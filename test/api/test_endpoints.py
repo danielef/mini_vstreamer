3f7d08ef1f37c28502e0e99b5c556fc319879c10
Daniel Estevez <daniel.ef@gmail.com>
2020-08-29 20:14:26 -0500

    Adds config test

--- test/api/test_endpoints.py
+++ test/api/test_endpoints.py
@@ -1,12 +1,14 @@
 import json
 
-from mini_vstreamer.app import initialize_app
+from mini_vstreamer.app import initialize_app, initialize_system
+from mini_vstreamer.api.defaults import system
 from flask import Flask
 
 app = Flask(__name__)
+initialize_system(system)
 initialize_app(app)
-client = app.test_client()
 
+client = app.test_client()
 
 def test_base():
     url = '/'
@@ -19,5 +21,12 @@ def test_not_found():
     url = '/not_found'
     response = client.get(url)
 
-    assert response.get_data().decode('utf-8') == 'ok'
-    assert response.status_code == 500
+    assert response.get_data().decode('utf-8') != 'ok'
+    assert response.status_code == 404
+
+def test_config():
+    url = '/api/config/default'
+    response = client.get(url)
+    
+    print(dir(response))
+    assert response.status_code == 200
