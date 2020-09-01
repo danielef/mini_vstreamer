69f511c9d32b6dc0239edbe80a912854359fa6cf
Daniel Estevez <daniel.ef@gmail.com>
2020-08-31 21:01:23 -0500

    Adds descriptions in config_model

--- src/mini_vstreamer/api/endpoints/configs.py
+++ src/mini_vstreamer/api/endpoints/configs.py
@@ -3,12 +3,10 @@ from mini_vstreamer.api.defaults import api, system
 
 ns = api.namespace('config', description='Config camera parameters')
 
-config_model = api.model('Camera config', { 'id' : fields.Integer(description=''), 
-                                            'name' : fields.String(description=''),
-                                            'fps' : fields.Integer(description=''),
-                                            'width' : fields.Integer(description=''),
-                                            'height' : fields.Integer(description='')})
-
+config_model = api.model('Camera config', { 'name' : fields.String(description='Camera Name'),
+                                            'fps' : fields.Integer(description='Camera Frames Per Second'),
+                                            'width' : fields.Integer(description='Camera Width Pixels'),
+                                            'height' : fields.Integer(description='Camera Height Pixels')})
 
 @ns.route('/<string:name>')
 class ConfigItem(Resource):
