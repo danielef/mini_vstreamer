3814cba54548c945cf3981c5df813312e1225f28
Daniel Estevez <daniel.ef@gmail.com>
2020-09-09 13:31:28 -0500

    Fix deprecated messages (safe_load)

--- src/mini_vstreamer/core/utils.py
+++ src/mini_vstreamer/core/utils.py
@@ -4,7 +4,7 @@ import yaml
 def read_yml_file(path):
     with open(path, 'rb') as config_file:
         try:
-            return yaml.load(config_file)
+            return yaml.safe_load(config_file)
         except yaml.YAMLError as exc:
             logging.info(exc)
             raise ValueError(exc)
