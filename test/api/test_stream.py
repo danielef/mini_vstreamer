b2e6c9bb1f820694f24abd9425521c75b1afe009
Daniel Estevez <daniel.ef@gmail.com>
2020-09-10 09:00:23 -0500

    Removes comments

--- test/api/test_stream.py
+++ test/api/test_stream.py
@@ -1,6 +1,5 @@
 from mini_vstreamer.api.defaults import system
 from mini_vstreamer.core.config import ConfigIO
-#from mini_vstreamer.core.stream.queue import Queue
 from mini_vstreamer.core.stream.queue import mutate_system as q_system
 
 def test_queue_factory():
