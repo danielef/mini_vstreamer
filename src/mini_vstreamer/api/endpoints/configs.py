0d914a7ea9d1e78eb0930526ed900c0ab6a186bc
Daniel Estevez <daniel.ef@gmail.com>
2020-09-10 09:03:00 -0500

    Adds video streamer

--- src/mini_vstreamer/api/endpoints/configs.py
+++ src/mini_vstreamer/api/endpoints/configs.py
@@ -1,5 +1,7 @@
+from flask import Response
 from flask_restx import fields, Resource
 from mini_vstreamer.api.defaults import api, system
+import cv2
 
 ns = api.namespace('camera', description='Camera parameters')
 ns_plural = api.namespace('cameras', description='All camera configurations')
@@ -37,3 +39,17 @@ class CameraItems(Resource):
 
     def get(self):
         return system['cameras']
+
+def encode(name):
+    while True:
+        encode_param = [int(cv2.IMWRITE_JPEG_QUALITY), 90]
+        result, img_encoded = cv2.imencode('.jpg', system['video'][name], encode_param)
+        contents = img_encoded.tobytes()
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + contents + b'\r\n')
+
+@ns.route('/<string:name>/video')
+class CameraVideo(Resource):
+
+    def get(self, name):
+        return Response(encode(name), mimetype='multipart/x-mixed-replace; boundary=frame')
