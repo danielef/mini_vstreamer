713ebd41ce2a4495e0355eeef7f02af4948fa1e7
Daniel Estevez <daniel.ef@gmail.com>
2020-09-09 20:56:31 -0500

    Adds Configurable and fixes deprecation messages

--- src/mini_vstreamer/core/config.py
+++ src/mini_vstreamer/core/config.py
@@ -1,9 +1,11 @@
-import collections
+from collections.abc import MutableMapping
+from mini_vstreamer.core.utils import read_yml_file
+
 import os
+import yaml
 
-from mini_vstreamer import read_yml_file
 
-class ConfigIO(collections.MutableMapping):
+class ConfigIO(MutableMapping):
 
     '''
     Mapping that works like a dict with storage in path
@@ -63,3 +65,70 @@ class ConfigIO(collections.MutableMapping):
 
     def getPath(self):
         return self.__path__
+
+
+class Configurable:
+
+    '''
+    Contract and generic methods of a YAML configurable item
+    '''
+    def __init__(self, index, configio, system=None, configContext=None):
+        self.__index__ = index
+        self.__configio__ = configio
+        self.__system__ = system
+        self.__configContext__ = configContext
+        self.__ommitCheckQIn__ = False
+        self.__ommitCheckQOut__ = False
+
+    def get(self, key=None, default=None):
+        return self.__configio__[self.__configContext__][self.__index__].get( key, default)
+
+    def set(self, key, value):
+        context = self.__configContext__
+        configio_copy = self.__configio__[context].copy()
+        configio_copy[self.__index__][key] = value
+        self.__configio__[context] = configio_copy
+
+    def delete(self, key):
+        context = self.__configContext__
+        configio_copy = self.__configio__[context].copy()
+        del configio_copy[self.__index__][key]
+        self.__configio__[context] = configio_copy
+
+    def configSlice(self):
+        return self.__configio__[self.__configContext__][self.__index__]
+
+    def setQueues(self):
+        if not get_with_default(self.__system__, 'queues', False):
+            return log.error('system without queues initialized')
+        in_queue_name = self.get('qIn', None)
+        out_queue_name = self.get('qOut', None)
+        context = self.__configContext__
+        name = self.get('name')
+
+        if self.__ommitCheckQIn__ == False:
+            if in_queue_name is None:
+                log.warn("Not found value for configio[\'{}\'][\'{}\'][\'qIn\']".format(context, name))
+            else:
+                in_queue = self.__system__['queues'].get(in_queue_name, None)
+                if in_queue is None:
+                    log.error("Not found queue in system[\'queues\'][\'{}\']".format(in_queue_name))
+                    log.error("configio[\'{}\'][\'{}\'][\'qIn\'] = \'{}\'".format(context, name, in_queue_name))
+                else:
+                    self.__inQ__ = in_queue
+
+        if self.__ommitCheckQOut__ == False:
+            if out_queue_name is None:
+                log.warn("Not found value for configio[\'{}\'][\'{}\'][\'qOut\']".format(context, name))
+            else:
+                out_queue = self.__system__['queues'].get(out_queue_name, None)
+                if out_queue is None:
+                    log.error("Not found queue in system[\'queues\'][\'{}\']".format(out_queue_name))
+                    log.error("configio[\'{}\'][\'{}\'][\'qOut\'] = \'{}\'".format(context, name, out_queue_name))
+                else:
+                    self.__outQ__ = out_queue
+
+    def withReplaceNone(self, key, replace):
+        current_value = self.get(key, None)
+        if current_value is None and replace is not None:
+            self.set(key, replace)
