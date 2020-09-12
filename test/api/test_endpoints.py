018ca8d282595aa2caff3634478ca5bf981365e8
Daniel Estevez <daniel.ef@gmail.com>
2020-09-12 18:56:33 -0500

    Fix config cases

--- test/api/test_endpoints.py
+++ test/api/test_endpoints.py
@@ -2,17 +2,17 @@ import json
 
 from mini_vstreamer.app import initialize_app
 from mini_vstreamer.api.defaults import app, system
+from mini_vstreamer.core.config import ConfigIO
+from mini_vstreamer.core.stream.queue import mutate_system as load_queues
+from mini_vstreamer.core.stream.camera import mutate_system as load_cameras
+
 from flask import Flask
 
-def initialize_system(system):
-    system['cameras'] = {}
-    system['cameras']['default'] = { 'name': 'default',
-                                     'fps': 0,
-                                     'width': 0,
-                                     'height': 0 }
-
-initialize_system(system)
+config = ConfigIO('./test/config-test.yml')
+load_queues(config)
+load_cameras(config)
 initialize_app(app)
+
 
 client = app.test_client()
 
@@ -34,25 +34,35 @@ def test_not_found():
 def test_cameras():
     url = '/api/cameras/'
     response = client.get(url)
-    assert response.json == system['cameras']
+    assert response.json == [{'name': 'default', 'videoURL': 0, 'defaultFPS': 2, 'qOut': 'frames'}]
     assert response.status_code == 200
 
 def test_camera():
     url = '/api/camera/default'
     response = client.get(url)
-    assert response.json == system['cameras']['default']
+    assert str(response.json) == str(system['cameras']['default'])
     assert response.status_code == 200
 
 def test_camera_update():
-    url = '/api/camera/default/fps/1'
-    response = client.put(url)
-    assert response.json['fps'] == 1
+    url = '/api/camera/default/defaultFPS'
+    response = client.put(url, data='1')
+    print('json: {}'.format(response.json))
+    assert response.json['defaultFPS'] == 1
+    assert response.status_code == 200
+
+    response = client.put(url, data='2')
+    assert response.json['defaultFPS'] == 2
     assert response.status_code == 200
 
 def test_camera_state():
     url = '/api/camera/default/start'
     response = client.post(url)
     assert system['cameras']['default'].status() == 'running'
+    assert response.status_code == 200
+
+    url = '/api/camera/default/stop'
+    response = client.post(url)
+    assert system['cameras']['default'].status() == 'stopped'
     assert response.status_code == 200
 
 def test_config_not_found():
